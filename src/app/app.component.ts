d52eebc7b4f6cfa4b9aeeb213fc45a1ac7c0dc3c
Vanessa Bustillos <vanebustillos1@gmail.com>
2024-01-24 00:43:52 -0500

    Import textbox-button component into the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextboxButtonComponent } from './textbox-button/textbox-button.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TextboxButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
